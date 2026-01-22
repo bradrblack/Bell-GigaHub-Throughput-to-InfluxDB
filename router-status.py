341d24a24fc096d3061ad7bf213d2048f6e9949c
bradrblack <66791904+bradrblack@users.noreply.github.com>
2026-01-22 10:27:32 -0500

    Fix typo in comment for router-status.py

--- router-status.py
+++ router-status.py
@@ -1,5 +1,5 @@
 #
-# This python script uses the sagemcom API to grab bytes sent and received from the Fiber and 10Gbaset-T port on a Bell GigaHub 2.0 and send the data to InfluxDB
+# This python script uses the sagemcom API to grab bytes sent and received from the Fiber and 10Gbase-T port on a Bell GigaHub 2.0 and send the data to InfluxDB
 # 
 # Based on: https://github.com/iMicknl/python-sagemcom-api 
 #
